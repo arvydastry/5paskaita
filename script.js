eafc25d22ed9ee1c28a8aa2b9ba587a9aa364d26
arvydastry <arvydas.trybe@gmail.com>
2022-05-04 22:06:58 +0300

    Keliu 5 paskaitos info

---
+++ script.js
@@ -0,0 +1,37 @@
+// const nameNode = document.getElementById("name");
+// nameNode.textContent = "Tomas";
+
+// const descriptionNode = document.getElementById("description");
+// document.getElementById("description"). textContent = "Man patinka programuoti";
+
+// document.body.style.backgroundColor = "f1f1f1";
+// nameNode.style.color = "blue";
+// descriptionNode.style.paddingLeft = "10px";
+// descriptionNode.style.padding = "10px";
+// descriptionNode.style.backgroundColor = "cyan";
+
+
+// const dliByTag = document.querySelector("li");
+// const liById = document.querySelector("#discount");
+// const lyByClass = document.querySelector(".yellow");
+
+// const firstItem = document.querySelector("li:first-child");
+// cont lastItem = document.querySelector("li:last-child");
+
+// const discountItems = document.querySelectorAll("#discount");
+// console.log(discountItems [0]);
+// console.log(discountItems. length);
+
+
+
+const imageSrc = 'https://crocoder.dev/icon.png';
+
+const divElement = document.querySelector('div');
+
+const imgElement = document.createElement('img');
+
+imgElement.src = imageSrc;
+
+divElement.appendChild(imgElement);
+
+
